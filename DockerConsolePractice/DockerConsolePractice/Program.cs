afeaf4f370d19a3208c8b3079a6fb2f7605f3ba5
parksh <tmdghks0906@gmail.com>
2025-01-28 19:59:05 +0900

    init

---
+++ DockerConsolePractice/DockerConsolePractice/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace DockerConsolePractice
+{
+    class Program
+    {
+        static void Main()
+        {
+            string path = "/app"; // Docker 컨테이너 내의 경로
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+                Console.WriteLine($"Directory created at {path}");
+            }
+            else
+            {
+                Console.WriteLine($"Directory 삭제 : {path}");
+                Directory.Delete(path, true); // true parameter enables recursive deletion
+            }
+
+            path = "/app/myfolder"; // Docker 컨테이너 내의 경로
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+                Console.WriteLine($"Directory created at {path}");
+            }
+            else
+            {
+                Console.WriteLine($"Directory 삭제 : {path}");
+                Directory.Delete(path, true); // true parameter enables recursive deletion
+            }
+        }
+    }
+}
